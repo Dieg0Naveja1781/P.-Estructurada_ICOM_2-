e485ca18289342f83794fdca1ba84995f360eebe
Diego A. Naveja L <150072802+Dieg0Naveja1781@users.noreply.github.com>
2024-01-31 20:04:50 -0600

    Update pruebagithub.cpp
    Primer cambio a un código

--- pruebagithub.cpp
+++ pruebagithub.cpp
@@ -1,32 +1,6 @@
 #include <iostream>
 
-int contador;
-int a = 1, x;
-
 int main ()
 {
-    for (int i = 0; i <= 50; i++)
-    {
-        std::cout<< "Contando... "<< i<< "\n";
-    }
-    
-    std::cout<< "\n""Ahora con una variable global/local: ";
-
-    for (contador = 0; contador <= 99; contador++)
-    {
-        std::cout<< "--> "<< contador<< "\n";
-    }
-
-    do
-    {
-        std::cout<< "Hola";
-        ++contador;
-        std::cout<< contador<<"\n";
-        ++contador;
-    } while (contador < 10);
-
-    std::cout<< "\n""El valor de i es: "<<contador;
-
-    x = contador + a;
-    
+   std::cout<< "Que tal profe, aquí probando el github" 
 }
